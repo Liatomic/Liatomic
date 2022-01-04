a64cc793667d14633eb777f885cd4c0d5c82e84f
RaviharaV <91451271+RaviharaV-bot@users.noreply.github.com>
2022-01-04 13:57:31 +0530

    Update about.ts

--- client/about.ts
+++ client/about.ts
@@ -10,7 +10,7 @@ export function aboutView(): VNode[] {
     ]
     return [
         h('div.about', [
-            h('img.center', { attrs: { src: `${model["asset-url"]}/favicon/favicon.png` } }),
+            h('img.center', { attrs: { src: `${model["asset-url"]}/favicon/static/favicon/istockphoto-1132090957-612x612.jpg` } }),
             h('h1', { attrs: { align: 'center' } }, _('About Liatomic')),
             h('p', _('Liatomic is a free, open-source atomic server derived from Liantichess.')),
             h('p', [
