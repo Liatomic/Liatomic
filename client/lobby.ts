c5d17c2c8f7be75acdc189c4dc23879441f98f39
RaviharaV <91451271+RaviharaV-bot@users.noreply.github.com>
2022-01-04 14:56:02 +0530

    Update lobby.ts

--- client/lobby.ts
+++ client/lobby.ts
@@ -928,7 +928,7 @@ export function lobbyView(model: PyChessModel): VNode[] {
         h('aside.sidebar-second', [ h('div#seekbuttons') ]),
         h('under-left', [
             h('a.reflist', { attrs: { href: 'https://discord.gg/5qvjPQstKS' } }, 'Discord'),
-            h('a.reflist', { attrs: { href: 'https://github.com/SriMethan/Liantichess' } }, 'Github'),
+            h('a.reflist', { attrs: { href: 'https://github.com/raviharav-bot/Liantichess' } }, 'Github'),
             h('a.reflist', { attrs: { href: '/faq' } }, _("FAQ")),
             h('a.reflist', { attrs: { href: '/stats' } }, _("Stats")),
             h('a.reflist', { attrs: { href: '/about' } }, _("About")),
