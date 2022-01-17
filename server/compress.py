28f092c829e0dcc6a9e741daadebe7d517d45aef
RaviharaV <91451271+RaviharaV-bot@users.noreply.github.com>
2022-01-17 08:08:18 +0530

    Update compress.py

--- server/compress.py
+++ server/compress.py
@@ -10,7 +10,7 @@ https://lichess.org/blog/Wqa7GiAAAOIpBLoY/developer-update-275-improved-game-com
 # Create mappings to compress variant, result and uci/usi move lists a little
 V2C = {
     "chess": "n",
-    "atomar": "A",
+    "atomar": "a",
     "capablanca": "c",
     "capahouse": "i",
     "crazyhouse": "h",
@@ -24,7 +24,7 @@ V2C = {
     "placement": "p",
     "seirawan": "s",
     "shogi": "g",
-    "minishogi": "a",
+#    "minishogi": "a",
     "shouse": "z",
     "sittuyin": "y",
     "xiangqi": "x",
