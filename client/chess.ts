1da5161690ae933deb535720103a4a78171ac37a
RaviharaV <91451271+RaviharaV-bot@users.noreply.github.com>
2022-01-21 19:56:31 +0530

    Update chess.ts

--- client/chess.ts
+++ client/chess.ts
@@ -309,13 +309,14 @@ export const VARIANTS: { [name: string]: Variant } = {
             icon: "♟",
           }),
           
-    antiatomic: new Variant({
-      name: "antiatomic", tooltip: () => _("capture all pieces."),
-      startFen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-      board: "standard8x8", piece: "standard",
-      pieceRoles: ["k", "q", "r", "b", "n", "p"],
-      enPassant: true,
-      chess960: true, icon: "♔", icon960: "♔",
+    antiatomic: new Variant({   
+        name: "antiatomic", tooltip: () => _("Atomic+Antichess"),
+        startFen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+        board: "standard8x8", piece: "standard",
+        pieceRoles: ["k", "q", "r", "b", "n", "p"],
+        enPassant: true,
+        pieceSound: "atomic",
+        chess960: true, icon: "~", icon960: "\\",
     }),
     
     racingkings: new Variant({
