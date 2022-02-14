36b014fda384748a3e3ffc524e2863f43585da4c
RaviharaV <91451271+RaviharaV-bot@users.noreply.github.com>
2022-02-14 14:16:35 +0530

    Update chess.ts

--- client/chess.ts
+++ client/chess.ts
@@ -285,8 +285,8 @@ export const VARIANTS: { [name: string]: Variant } = {
     
     epicatomic: new Variant({
         name: "epicatomic", tooltip: () => _("atomic but epic"),
-        startFen: "rnnrqkrnnr/pppppppppp/10/10/10/10/PPPPPPPPPP /RNNRQKRNNR w KQkq - 0 1",
-        board: "standard10x10", piece: "standard",
+        startFen: "rnnrqkrnnr/pppppppppp/10/8/10/8/PPPPPPPPPP/RNNRQKRNNR w KQkq - 0 1",
+        board: "standard10x8", piece: "standard",
         pieceRoles: ["k", "q", "r", "b", "n", "p"],
         enPassant: false,
         pieceSound: "atomic",
