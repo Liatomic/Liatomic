ba1f02853d7b6997870f032ea4df8802e46583d7
RaviharaV <91451271+RaviharaV-bot@users.noreply.github.com>
2022-02-15 11:36:22 +0530

    Liatomicdev (#8)
    * Update chess.ts
    
    * Update chess.ts
    
    * Update compress.py
    
    * Update const.py
    
    * Update variants.ini
    
    * Update const.py
    
    * Update chess.ts
    
    * Update variants.ini
    
    * Update chess.ts
    
    * Update variants.ini
    
    * Update variants.ini
    
    * Update variants.ini
    
    * Update chess.ts
    
    * Update chess.ts
    
    * Update variants.ini
    
    * Update variants.ini
    
    * Update chess.ts
    
    * Try to Fix epicatomic
    
    * Update chess.ts
    
    * Update variantsIni.ts
    
    * Update variants.ini
    
    * Update chess.ts
    
    * Update variantsIni.ts
    
    * Update variants.ini
    
    * Update variants.ini
    
    * Update chess.ts
    
    * Update variantsIni.ts
    
    * Update variants.ini
    
    * Update chess.ts
    
    * Update variantsIni.ts
    
    * Update variants.ini
    
    * Update variantsIni.ts
    
    * Update variantsIni.ts
    
    * Update chess.ts
    
    * Update variants.ini
    
    * Revert "10x10"
    
    * Update chess.ts
    
    * Update variantsIni.ts
    
    * Update variants.ini
    
    * Fix Rooks Don't Move
    
    * revert
    
    * Update chess.ts
    
    * Update variantsIni.ts
    
    * Update variants.ini
    
    * Revert "Fen"
    
    * Revert "Normal2epic"
    
    Co-authored-by: SriMethan <methanroyal@gmail.com>

--- client/variantsIni.ts
+++ client/variantsIni.ts
@@ -10,6 +10,25 @@ capturesToHand = true
 pocketSize = 6
 castling = false
 
+[epicatomic:atomic]
+variantTemplate = atomic
+king = k
+queen = q
+bishop = b
+knight = n
+rook = r
+pawn = p
+promotionPieceTypes = b
+blastOnCapture = true
+castling = false
+startFen = rnnbqkbnnr/pppppppppp/10/10/10/10/PPPPPPPPPP/RNNBQKBNNR[] w KQkq - 0 1
+maxRank = 8
+maxFile = 10
+chess960 = false
+promotionRank = 6
+doubleStep = true
+castling = false
+
 [atomicrk:atomic]
 startFen = 8/8/8/8/8/8/krbnNBRK/qrbnNBRQ w - - 0 1
 flagPiece = k
